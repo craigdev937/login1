b34bb970cf48dac1653b45cd6740608ebcbe2538
Craig <indigomx9@gmail.com>
2021-07-19 16:36:48 -0400

    I added the User Model and the Auth file.

---
+++ src/middleware/Auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+
+export const Auth = async (req, res, next) => {
+    try {
+        const token = req.bookies.jwtoken;
+        const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+        const rootUser = await User.findOne({
+            _id: verifyToken._id,
+            "tokens.token": token
+        });
+        if (!rootUser) {
+            throw new Error("User is not found!");
+        };
+        req.token = token;
+        req.rootUser = rootUser;
+        req.userID = rootUser._id;
+        next();
+    } catch (error) {
+        res.status(401).send("Unauthorized - No Token!");
+        console.log(error);
+    }
+};
+
+
+
+
+
+
---
+++ src/models/User.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    work: { type: String, required: true },
+    password: { type: String, required: true },
+    cpassword: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    messages: [{
+        type: String, required: true,
+        email: String, required: true,
+        phone: String, required: true,
+        message: String, required: true,
+    }],
+    tokens: [{
+        token: { type: String, required: true }
+    }]
+});
+
+// Hash the Password
+UserSchema.pre("save", async function(next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    };
+    next();
+});
+
+// Generate the Token
+UserSchema.methods.generateAuthtoken = async function() {
+    try {
+        let token = jwt.sign({_id: this._id}, process.env.JWT_SECRET);
+        this.tokens = this.tokens.concat({ token: token });
+        await this.save();
+        return token;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Storing Messages
+UserSchema.methods.addmessage = 
+async function(name, email, phone, message) {
+    try {
+        this.messages = this.messages.concat({
+            name, email, phone, message
+        });
+        await this.save();
+        return this.messages;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const User = mongoose.model("User", UserSchema);
+
+
+
+
+
